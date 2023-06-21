41b8cc647cbf166be958d0c206d5e95acc612eb8
fabigazi <fgaziano98@gmail.com>
2023-06-21 10:51:32 -0400

    all working now

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   tuit: String,
+  username: String,
+  handle: String,
+  image: String,
+  time: String,
   likes: Number,
   liked: Boolean,
 }, {collection: 'tuits'});
